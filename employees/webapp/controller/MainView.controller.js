49df8d96df5c59e88802d2d47920b35cbe343108
smusicpia <s_musicpia@yahoo.com.mx>
2024-04-16 23:36:55 +0000

    04 Elementos y controladores UI5

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -7,9 +7,25 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
+        function myCheck() {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+            if (valueEmployee.length ===6){
+                //inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            } else {
+                //inputEmployee.setDescription("Not OK");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+            };
+        }
+
         return Controller.extend("logaligroup.employees.controller.MainView", {
             onInit: function () {
 
-            }
+            },
+
+            onValidate: myCheck
         });
     });

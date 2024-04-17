d0d98ef75a39892586a1718fddeba208e807f954
smusicpia <s_musicpia@yahoo.com.mx>
2024-04-17 17:17:05 +0000

    05 Metodos y Eventos Commit Project Fiori

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -1,13 +1,19 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {type sap.ui.model.FilterOperator} FilterOperator
      */
-    function (Controller) {
+    function (Controller, JSONModel, Filter, FilterOperator) {
         "use strict";
 
-        function myCheck() {
+        /* function myCheck() {
             var inputEmployee = this.byId("inputEmployee");
             var valueEmployee = inputEmployee.getValue();
             if (valueEmployee.length ===6){
@@ -19,13 +25,64 @@ sap.ui.define([
                 this.getView().byId("labelCountry").setVisible(false);
                 this.getView().byId("slCountry").setVisible(false);
             };
+        }; */
+
+        function onInit() {
+            var oJSONModel = new JSONModel();
+            var oView = this.getView();
+            /* var i18nBundle = oView.getModel("i18n").getResourceBundle();
+            var oJSON = {
+                employeeId: "123456",
+                countryKey: "UK",
+                listCountry: [
+                    {
+                        key: "US",
+                        text: i18nBundle.getText("countryUS")
+                    },
+                    {
+                        key: "UK",
+                        text: i18nBundle.getText("countryUK")
+                    },
+                    {
+                        key: "ES",
+                        text: i18nBundle.getText("countryES")
+                    }
+                ]
+            };
+            oJSONModel.setData(oJSON); */
+            oJSONModel.loadData("./localService/mockdata/Employees.json", false);
+            // Validar en la consola, que se esten cargando los datos del fichero
+            /* oJSONModel.attachRequestCompleted(function (oeventModel) {
+                console.log(JSON.stringify(oJSONModel.getData()));
+            }); */
+            oView.setModel(oJSONModel);
+        };
+
+        function onFilter() {
+            var oJSON = this.getView().getModel().getData();
+            var filters = [];
+            if (oJSON.EmployeeId !== "") {
+                filters.push(new Filter("EmployeeID", FilterOperator.EQ, oJSON.EmployeeId));
+            }
+            if (oJSON.CountryKey !== "") {
+                filters.push(new Filter("Country", FilterOperator.EQ, oJSON.CountryKey));
+            }
+            // Update Table
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
+        };
+
+        function onClearFilter() {
+            var oModel = this.getView().getModel();
+            oModel.setProperty("/EmployeeId", "");
+            oModel.setProperty("/CountryKey", "");
         }
 
-        return Controller.extend("logaligroup.employees.controller.MainView", {
-            onInit: function () {
-
-            },
-
-            onValidate: myCheck
-        });
+        const Main = Controller.extend("logaligroup.employees.controller.MainView", {});
+        //Main.prototype.onValidate = myCheck;
+        Main.prototype.onInit = onInit;
+        Main.prototype.onFilter = onFilter; 
+        Main.prototype.onClearFilter = onClearFilter;
+        return Main;
     });
